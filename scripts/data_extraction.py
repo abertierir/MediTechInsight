47a85425e274c0d981fbdfa111ac1cc493a87ad9
abertierir <abertierir@unal.edu.co>
2023-09-27 04:24:10 -0500

    Adding fucntion to read the response

--- scripts/data_extraction.py
+++ scripts/data_extraction.py
@@ -8,12 +8,44 @@ class APIDataIngestor:
         self.base_url = config_data.get("api.url")
         self.api_key = config_data.get("api.key")
     
-    def setHeader(self):
+    def set_header(self):
         self.headers = {
             'accept': 'application/json',
-            'X-cb-user-key':self.apy_key,
+            'X-cb-user-key':self.api_key,
             'Content-Type': 'application/json'
         }
+
+    def set_body(self):
+        self.body={
+            "field_ids": [
+                "identifier",
+                "location_identifiers",
+                "short_description"
+                ],
+            "query": [
+                {
+                    "type": "predicate",
+                    "field_id": "short_description",
+                    "operator_id": "contains",
+                    "values": ["SaaS"]
+                }
+            ],
+         "limit": 50
+        }
+
+    def post_request(self):
+
+        self.response=requests.post(self.base_url)
+        
+        if self.response.status_code == 200:
+            data = self.response.json()
+            
+            with open('../data/crunchbase_data.json', 'w') as json_file:
+                json.dump(data, json_file, indent=4)
+                print("Data has been saved in: '/data/crunchbase_data.json'")
+        else:
+            print(f"Error {self.response.status_code}: Couldn't fetch data from Crunchbase.")
+
     
     def fetch_organization_data(self, organization_id):
         endpoint = f"organizations/{organization_id}"
