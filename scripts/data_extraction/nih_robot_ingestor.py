25ae30f53373bb16d24d9ff95f424df003637854
abertierir <abertierir@unal.edu.co>
2023-10-15 10:37:45 -0500

    Avoiding execution problem when It doesn't find results

--- scripts/data_extraction/nih_robot_ingestor.py
+++ scripts/data_extraction/nih_robot_ingestor.py
@@ -5,6 +5,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import TimeoutException
 import csv
 
 import re
@@ -50,20 +51,41 @@ class NIHFormIngestion:
             else:
                 print("No se encontró el search box")
 
+            self.resultsPerPage(50)
+
             self.getAllResults(termino)
 
         self.driver.quit()
 
+    def resultsPerPage(self, number):
+        try:
+            # Espera hasta que el enlace "Results Per Page" sea clickeable y hágale clic
+            results_per_page_link = WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable((By.ID, "results-per-page"))
+            )
+            results_per_page_link.click()
+
+            # Espera hasta que la opción "50" sea clickeable y hágale clic
+            option_50 = WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, "ul#page-size-options li a[href*='page_size=50']"))
+            )
+            option_50.click()
+        except TimeoutException:
+            # Maneja la excepción si se lanza
+            print("No hubo resultados de búsqueda")
+
     def getAllResults(self, termino):
+        self.driver.refresh()
         resultados= self.driver.find_elements(By.CSS_SELECTOR,".resultRow.no-padding h3 a")
         total=len(resultados)
 
         # Primero hay que saber cuantas páginas
         print("Término de búsqueda: ", termino)
         print("Total elementos encontrados en la primera página: ",total)
+        self.driver.refresh()
         pagination=self.driver.find_elements(By.CSS_SELECTOR,".bottom-pagination a")
 
-        enlace_maximo = None
+    
         valor_maximo = 0
 
         for enlace in pagination:
@@ -72,15 +94,14 @@ class NIHFormIngestion:
                 numero = int(texto)
                 if numero > valor_maximo:
                     valor_maximo = numero
-                    enlace_maximo = enlace
             except ValueError:
                 pass
 
         if(valor_maximo==0):
             valor_maximo=1
-            self.getAllCodesBySearchTerm(valor_maximo)
+            self.getAllCodesBySearchTerm(valor_maximo,termino)
         else:
-            self.getAllCodesBySearchTerm(valor_maximo)
+            self.getAllCodesBySearchTerm(valor_maximo, termino)
         
         resultados.clear()
 
@@ -90,14 +111,13 @@ class NIHFormIngestion:
         except ValueError:
             return 0  # Si no es un número, devuelve 0
     
-    def getAllCodesBySearchTerm(self, valor_maximo):
+    def getAllCodesBySearchTerm(self, valor_maximo, termino):
         timeout = 30  # Tiempo máximo de espera en segundos
         wait = WebDriverWait(self.driver, timeout)
 
         print("Total páginas", valor_maximo)
-
-        pagination=self.driver.find_elements(By.CSS_SELECTOR,".bottom-pagination a")
-        self.printResults()
+        
+        self.printResults(termino)
 
         if valor_maximo>1:
             for i in range(2, valor_maximo + 1):
@@ -105,13 +125,11 @@ class NIHFormIngestion:
                 enlace_siguiente = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".pagination-button.right-arrow")))
                 enlace_siguiente.click()
                 self.driver.implicitly_wait(20)
-                self.printResults()
+                self.printResults(termino)
                 
 
-    def printResults(self):
-
-        with open('primary_di_numbers.csv', mode='w', newline='') as file:
-            writer = csv.writer(file)
+    def printResults(self, termino):
+        resultados = None 
         # Espera hasta que todos los elementos se carguen
         timeout = 30  # Tiempo máximo de espera en segundos
         wait = WebDriverWait(self.driver, timeout)
@@ -122,17 +140,23 @@ class NIHFormIngestion:
             # Si se produce el error, esperar un breve momento y volver a intentar
             time.sleep(2)  # Esperar 2 segundos
             resultados = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".resultRow.no-padding h3 a")))
-        
+        except TimeoutException:
+            # Maneja la excepción si se lanza
+            print("No hubo resultados de búsqueda")
         
         if resultados:
             for result in resultados:
-                partes = result.text.rsplit(" - ", 1)
-                if len(partes) == 2:
-                    id = partes[1]
-                    print("El id : ", id)
-                    writer.writerow([id])
-                else:
-                    print("No se encontró un código en la cadena:", result.text)
+                
+                with open('data/primary_di_numbers.csv', mode='a', newline='') as file:
+                    writer = csv.writer(file)
+
+                    partes = result.text.rsplit(" - ", 1)
+                    if len(partes) == 2:
+                        id = partes[1]
+                        print("El id : ", str(id))
+                        writer.writerow([str(id),termino])
+                    else:
+                        print("No se encontró un código en la cadena:", result.text)
 
 
 
